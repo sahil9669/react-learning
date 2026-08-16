60adf5a7ddb6b0ffb9083574f5cc329d8605d6a4
Sahil Patil <sahilpatil9889@gmail.com>
2026-08-16 19:45:45 +0530

    Set up Tailwind CSS and add basic React styling

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-cyan-900 h-screen text-white text-3xl font-bold flex justify-center items-center'>
+      Intro to basic tailwnd and configuration
+    </div>
+  )
+}
+
+export default App
